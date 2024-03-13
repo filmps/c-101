fd35a25886937a7a028d12f335208d8dace61b33
filmps <97680543+filmps@users.noreply.github.com>
2024-03-12 19:28:24 -0700

    Create 101.cs

---
+++ 101.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyProgram
+{
+    class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.writeline("Hello world");
+        }
+    }
+}
